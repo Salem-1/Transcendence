9e4a3f196e25a09062f64c8193bb4a988fd8d3ee
Ahmed Salem <ahsalem@lab3r6s5.42abudhabi.ae>
2023-08-06 20:14:58 +0400

    added jquery

--- sandbox/Drum Kit Starting Files/index.js
+++ sandbox/Drum Kit Starting Files/index.js
@@ -4,20 +4,22 @@ for (i = 0; i < document.querySelectorAll(".drum").length; i++) {
     document.querySelectorAll(".drum")[i].addEventListener("click", 
     function() {
         var buttonInnerHTML = this.innerHTML;
-        
+        animation(buttonInnerHTML);
     });
     document.addEventListener("keydown", function(event) 
     {
+		console.log(event);
         makesound(event.key);
-    });
+		animation(event.key);
+	});
 }
 
 function makesound(key) {
     switch (key) {
         case "w":
             var tom1 = new Audio("sounds/tom-1.mp3");
-            tom1.play();
-            break;
+            tom1.play() ;
+            break ;
         case "a":
             var tom2 = new Audio("sounds/tom-2.mp3");
             tom2.play();
@@ -42,10 +44,23 @@ function makesound(key) {
             var kick = new Audio("sounds/kick-bass.mp3");
             kick.play();
             break;
+        case "Alt":
+            var kick = new Audio("sounds/kick-bass.mp3");
+            kick.play();
+            break;
         default:
             console.log(buttonInnerHTML);
 }
 }
 handle_click = () => {
     alert("I got clicked!");
+}
+
+function animation(currentKey)
+{
+	var activeButton = document.querySelector("." + currentKey);
+	activeButton.classList.add("pressed");
+	setTimeout(function() {
+		activeButton.classList.remove("pressed");
+	}, 100);
 }
