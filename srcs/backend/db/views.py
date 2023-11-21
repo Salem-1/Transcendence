12b227b15e73085de2e9f80dc91b7cddc68470f7
Salem-1 <ph.ahmedsalem@gmail.com>
2023-11-21 17:13:48 +0400

    backup before refractoring intra auth function

--- srcs/backend/db/views.py
+++ srcs/backend/db/views.py
@@ -5,7 +5,8 @@ from django.http  import JsonResponse
 from django.views.decorators.csrf import csrf_exempt
 from django.views.decorators.http import require_POST
 import json
-
+import requests
+import os
 
 @csrf_exempt
 def register_user(request):
@@ -37,7 +38,7 @@ def login_user(request):
         user = authenticate(request, username=username, password=password)
         if user is not None:
             login(request, user)
-            return JsonResponse({'message': 'Login Successful'})
+            return JsonResponse({'message': username})
         else:
             return JsonResponse({'error': 'Invalid request username or password'}, status=401)
     return JsonResponse({}, status=400)  
@@ -47,6 +48,57 @@ def auth_intra(request):
     if request.method =='POST':
         data = json.loads(request.body)
         code  = data.get('code')
-        print(f"code {code}");
-        if code:
-            return JsonResponse({'message': 'Login Successful'})
+        if not code:
+            return JsonResponse({'error': 'Intra authentication failed'}, status=400)
+        try:
+            url = "https://api.intra.42.fr/oauth/token"
+            data = {
+                'grant_type': 'authorization_code',
+                'client_id': os.getenv('intra_client_id'),
+                'client_secret': os.getenv('intra_client_secret'),
+                'code': code,
+                'redirect_uri': 'http://localhost:3000/auth',
+            }
+            response = requests.post(url, data=data)
+
+            if response.status_code != 200:
+                print("response:", response.json()) 
+                print("Access Token:", response.json()['access_token'])
+
+                bearer_token =  "Bearer " +response.json()['access_token']
+                headers = {
+                    "Authorization":  bearer_token,
+                }
+                url = "https://api.intra.42.fr/v2/me"
+                response = requests.get(url, headers=headers)
+                username = response.json()['email']
+                password = os.getenv('secret_pass')
+                if not username:
+                    return JsonResponse({'message': response.json()['access_token']}, status=500)
+
+                print("\n\n")
+                if response.status_code == 200:
+                    print(f"\n\nrecieved email = {username}")
+                    if User.objects.filter(username=username).exists():
+                        user = authenticate(request, username=username, password=password)
+                        if user is not None:
+                            login(request, user)
+                            print(f"Successful login via intra {username} finally Alhamdolelah")
+                            print(f"Successful login via intra {response.json()} finally Alhamdolelah")
+                            return JsonResponse({'message': username })
+                    else:
+                        user = User.objects.create_user(username=username, password=password)
+                        user = authenticate(request, username=username, password=password)
+                        if user is not None:
+                            login(request, user)
+                            return JsonResponse({'message': username + 'Login Successful'})
+                    return JsonResponse({'error': "couldn't register or login " + response.json()['email']})
+                else:
+                    print(f"Recieved error in the second call {response.status_code}");
+                return JsonResponse({'message': response.json()['access_token']}, status=500)
+            else:
+                print(f"error code {response.status_code}")
+        except Exception as e:
+            print(f"error {e}")
+            return JsonResponse({'error': f"Internal server error"}, status=500)
+    return JsonResponse({'error': "Internal server error"}, status=500)
