01507b72f0ec5464a5890e31c58fc382f68ed8cb
mkhan <mkhan@student.42.fr>
2023-12-25 22:20:21 +0400

    (add) tournament/singleplayer landing page wih the user dropdown

---
+++ srcs/frontend/dropDown.js
@@ -0,0 +1,15 @@
+function myFunction()
+{
+  document.getElementById("myDropdown").classList.toggle("show");
+}
+window.onclick = function(e)
+{
+	if (!e.target.matches('#Person'))
+	{
+		var myDropdown = document.getElementById("myDropdown");
+		if (myDropdown.classList.contains('show'))
+		{
+			myDropdown.classList.remove('show');
+		}
+	}
+}
