9c28f36f9f254fef30caf65f3b7f5adf0a96dad8
mkhan <mkhan@student.42.fr>
2023-12-27 13:25:16 +0400

    basic pop up

--- srcs/frontend/dropDown.js
+++ srcs/frontend/dropDown.js
@@ -13,3 +13,19 @@ window.onclick = function(e)
 		}
 	}
 }
+
+var modal = document.getElementById("myModal");
+var btn = document.getElementById("tournament");
+var span = document.getElementsByClassName("close")[0];
+
+btn.onclick = function() {
+	modal.style.display = "block";
+}
+span.onclick = function() {
+	modal.style.display = "none";
+}
+window.onclick = function(event) {
+	if (event.target == modal) {
+		modal.style.display = "none";
+	}
+}
