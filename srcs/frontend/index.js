7c3e3eabcaa217ada0cf4e9af980dc35fa2b146c
MahdyZ7 <ayassin@MahdyZ7>
2023-12-27 12:49:28 +0400

    single page aplication that loads elements as needed but does not reuse them

--- srcs/frontend/index.js
+++ srcs/frontend/index.js
@@ -1,169 +1,173 @@
-
-
 async function register() {
-    const username = document.getElementById('username').value;
-    const password = document.getElementById('password').value;
-    const confirmPassword = document.getElementById('confirmpassword').value;
+	const username = document.getElementById("username").value;
+	const password = document.getElementById("password").value;
+	const confirmPassword = document.getElementById("confirmpassword").value;
 
-    
-    if (!isValidRegeistrationIput(username, password, confirmPassword))
-        return ;
-    try {
-      const response = await fetch('http://localhost:8000/register/', {
-        method: "POST",
-        headers: {
-          'Content-Type': 'application/json',
-        },
-        body: JSON.stringify({ username,  password}),
-      });
+	if (!isValidRegeistrationIput(username, password, confirmPassword)) return;
+	try {
+		const response = await fetch("http://localhost:8000/register/", {
+			method: "POST",
+			headers: {
+				"Content-Type": "application/json",
+			},
+			body: JSON.stringify({ username, password }),
+		});
 
-      const result = await response.json();
+		const result = await response.json();
 
-      if (response.ok) {
-        alert('Registration successful! Now you can log in.');
-        window.location.href = 'login.html';
-      } else {
-        alert(`Registration failed: ${result.error}`);
-      }
-    } catch (error) {
-      console.error('Error during registration:', error);
-      alert(`Registration failed: ${error}`);
-    }
+		if (response.ok) {
+			alert("Registration successful! Now you can log in.");
+			callRoute("/login");
+			// window.location.href = "login.html";
+		} else {
+			alert(`Registration failed: ${result.error}`);
+		}
+	} catch (error) {
+		console.error("Error during registration:", error);
+		alert(`Registration failed: ${error}`);
+	}
 }
-
 
 async function login() {
-    const username = document.getElementById('username').value;
-    const password = document.getElementById('password').value;
+	const username = document.getElementById("username").value;
+	const password = document.getElementById("password").value;
 
-    if (!isValidLoginIput(username, password))
-      return ;
-    try {
-      const response = await fetch('http://localhost:8000/login/', {
-        method: "POST",
-        headers: {
-          'Content-Type': 'application/json',
-        },
-        body: JSON.stringify({ username, password }),
-      });
+	if (!isValidLoginIput(username, password)) return;
+	try {
+		const response = await fetch("http://localhost:8000/login/", {
+			method: "POST",
+			headers: {
+				"Content-Type": "application/json",
+			},
+			body: JSON.stringify({ username, password }),
+		});
 
-      const result = await response.json();
+		const result = await response.json();
 
-      if (response.status == 200 && result.jwt_token){ 
-        await storeJWTInCookies(result);
-        alert(`Successful! log in welcome ${username}.`);
-        window.location.href = 'landing.html';
-      } else if (response.status == 302 && result.type == "otp"){
-        
-          double_factor_authenticate(result);
-      }
-      else{
-          alert(`Login failed: ${result.error}`);
-      }
-    } catch (error) {
-      console.error('Error during registration:', error);
-      alert(`Error during registration: ${error}`);
-    }
-  }
-
-function isValidRegeistrationIput(username, password, confirmPassword)
-{
-    
-    if (username.length < 1)
-      alert("Registration failed: Choose longer username");
-    else if ((password.length < 8))
-        alert("Passwords too short, should be 8 cahr at leaset");
-    else if (/[ !@#$%^&*(),.;?":{}|<>' ]/.test(username))
-      alert("Registration failed: Username cannot contain  those characters !@#$%^&*,.?\":;{} ' ' |<>'");
-      else if (!(/[A-Z]/.test(password) && /[a-z]/.test(password) && /\d/.test(password)))
-      {
-        alert("Registration failed: password must contain at least one upper, lower case letters and number");
-      }
-      
-      else if (password !== confirmPassword)
-      alert("Registration failed: Passwords do not match");
-      else
-      return (true);
-      return (false);   
-    }
-    
-    function isValidLoginIput(username, password)
-    {
-      if (!(username.length > 1 && (password.length > 7) && !(/[ !@#$%^&*(),.;?":{}|<>' ]/.test(username))))
-      {  
-        alert("Invalid username");
-        return (false);
-      }
-      else if (!(/[A-Z]/.test(password) && /[a-z]/.test(password) && /\d/.test(password)))
-      {
-        alert("Login failed: password must contain at least one upper, lower case letters and number");
-        return (false);
-      }
-    return (true);   
+		if (response.status == 200 && result.jwt_token) {
+			await storeJWTInCookies(result);
+			alert(`Successful! log in welcome ${username}.`);
+			callRoute("/home");
+			// window.location.href = "landing.html";
+		} else if (response.status == 302 && result.type == "otp") {
+			double_factor_authenticate(result);
+		} else {
+			alert(`Login failed: ${result.error}`);
+		}
+	} catch (error) {
+		console.error("Error during registration:", error);
+		alert(`Error during registration: ${error}`);
+	}
 }
 
-async function  storeJWTInCookies(result)
-{
-  // Assuming 'response' is your fetch response
-  //extract "jwt_token" from response body
-  const jwt_token = result.jwt_token;
-  if (!jwt_token)
-    return (false);
-  document.cookie = `Authorization=Bearer ${jwt_token}; Secure; SameSite=Strict`;
-  return (true);
+function isValidRegeistrationIput(username, password, confirmPassword) {
+	if (username.length < 1)
+		alert("Registration failed: Choose longer username");
+	else if (password.length < 8)
+		alert("Passwords too short, should be 8 cahr at leaset");
+	else if (/[ !@#$%^&*(),.;?":{}|<>' ]/.test(username))
+		alert(
+			"Registration failed: Username cannot contain  those characters !@#$%^&*,.?\":;{} ' ' |<>'"
+		);
+	else if (
+		!(
+			/[A-Z]/.test(password) &&
+			/[a-z]/.test(password) &&
+			/\d/.test(password)
+		)
+	) {
+		alert(
+			"Registration failed: password must contain at least one upper, lower case letters and number"
+		);
+	} else if (password !== confirmPassword)
+		alert("Registration failed: Passwords do not match");
+	else return true;
+	return false;
 }
 
-
-function  addJWTToRrequest()
-{
-    const token = localStorage.getItem('jwtToken');
-  fetch('/some-protected-endpoint', {
-    headers: {
-      'Authorization': `Bearer ${token}`,
-      'Content-Type': 'application/json',
-    },
-  })
-
+function isValidLoginIput(username, password) {
+	if (
+		!(
+			username.length > 1 &&
+			password.length > 7 &&
+			!/[ !@#$%^&*(),.;?":{}|<>' ]/.test(username)
+		)
+	) {
+		alert("Invalid username");
+		return false;
+	} else if (
+		!(
+			/[A-Z]/.test(password) &&
+			/[a-z]/.test(password) &&
+			/\d/.test(password)
+		)
+	) {
+		alert(
+			"Login failed: password must contain at least one upper, lower case letters and number"
+		);
+		return false;
+	}
+	return true;
 }
 
-
-async function  double_factor_authenticate(result)
-{
-  await storeJWTInCookies(result);
-  const otp = prompt("Enter 6 digits OTP from your authenticator app:", "000000");
-    const otpPattern = /^\d{6}$/;
-    if (otpPattern.test(otp)) {
-      try{
-        const response = await fetch('http://localhost:8000/double_factor_auth/', {
-          method: "POST",
-          headers: {
-            'Content-Type': 'application/json',
-          },
-          body: JSON.stringify({otp}),
-          credentials: 'include'
-        });
-
-        const result = await response.json();
-
-        if (response.ok){ 
-          await storeJWTInCookies(result);
-          alert(`Successful! log in welcome .`);
-          window.location.href = 'landing.html';
-        }
-          else{
-          alert(`Wrong OTP 🥲`);
-          }
-      } catch (error) {
-        console.log('Error during registration:', error);
-        alert(`Error during registration: ${error}`);
-      }
-      } else {
-      alert("Invalid OTP. Please enter a 6-digit numeric code.");
-
-    }
+async function storeJWTInCookies(result) {
+	// Assuming 'response' is your fetch response
+	//extract "jwt_token" from response body
+	const jwt_token = result.jwt_token;
+	if (!jwt_token) return false;
+	document.cookie = `Authorization=Bearer ${jwt_token}; Secure; SameSite=Strict`;
+	return true;
 }
 
+function addJWTToRrequest() {
+	const token = localStorage.getItem("jwtToken");
+	fetch("/some-protected-endpoint", {
+		headers: {
+			Authorization: `Bearer ${token}`,
+			"Content-Type": "application/json",
+		},
+	});
+}
 
+async function double_factor_authenticate(result) {
+	await storeJWTInCookies(result);
+	const otp = prompt(
+		"Enter 6 digits OTP from your authenticator app:",
+		"000000"
+	);
+	const otpPattern = /^\d{6}$/;
+	if (otpPattern.test(otp)) {
+		try {
+			const response = await fetch(
+				"http://localhost:8000/double_factor_auth/",
+				{
+					method: "POST",
+					headers: {
+						"Content-Type": "application/json",
+					},
+					body: JSON.stringify({ otp }),
+					credentials: "include",
+				}
+			);
+
+			const result = await response.json();
+
+			if (response.ok) {
+				await storeJWTInCookies(result);
+				alert(`Successful! log in welcome .`);
+				callRoute("/home");
+				// window.location.href = "landing.html";
+			} else {
+				alert(`Wrong OTP 🥲`);
+			}
+		} catch (error) {
+			console.log("Error during registration:", error);
+			alert(`Error during registration: ${error}`);
+		}
+	} else {
+		alert("Invalid OTP. Please enter a 6-digit numeric code.");
+	}
+}
 
 /**
  * peer w-full h-full min-h-[100px] bg-transparent text-blue-gray-700 font-sans font-normal outline outline-0 focus:outline-0 resize-y disabled:bg-blue-gray-50 disabled:border-0 disabled:resize-none transition-all placeholder-shown:border placeholder-shown:border-blue-gray-200 placeholder-shown:border-t-blue-gray-200 border focus:border-2 border-t-transparent focus:border-t-transparent text-sm px-3 py-2.5 rounded-[7px] border-blue-gray-200 focus:border-gray-900 min-h-full !border-0 focus:border-transparent
---
+++ srcs/frontend/routing.js
@@ -0,0 +1,112 @@
+const urlPageTitle = "Pong Game";
+
+// // create document click that watches the nav links only
+// document.addEventListener("click", (e) => {
+// 	const { target } = e;
+// 	console.log("target: ", target);
+// 	e.preventDefault();
+// 	// return;
+// 	if (!target.matches("nav a") && !target.matches("btns a")) {
+// 		return;
+// 	}
+// 	e.preventDefault();
+// 	route();
+// });
+
+// create an object that maps the url to the template, title, and description
+const urlRoutes = {
+	404: {
+		template: "/404.html",
+		title: "404 | " + urlPageTitle,
+		description: "Page not found",
+	},
+	"/": {
+		template: "/templates/landing-page-body.html",
+		title: "Home | " + urlPageTitle,
+		description: "This is the home page",
+		theme: "/css/style.css",
+	},
+	"/frontend/": {
+		template: "/templates/landing-page-body.html",
+		title: "Home | " + urlPageTitle,
+		description: "This is the home page",
+		theme: "/css/style.css",
+	},
+	"/login": {
+		template: "/templates/login-body.html",
+		title: "Login | " + urlPageTitle,
+		description: "This is the login page",
+		theme: "/css/login.css",
+	},
+	"/register": {
+		template: "/templates/registration-body.html",
+		title: "Registration | " + urlPageTitle,
+		description: "This is the registration page",
+		theme: "/css/registration.css",
+	},
+	"/home": {
+		template: "/templates/home-page-body.html",
+		title: "Home | " + urlPageTitle,
+		description: "This is the Home page",
+		theme: "/css/style.css",
+		script: "greet.js",
+	},
+	"/game": {
+		template: "/templates/game-page-body.html",
+		title: "Game | " + urlPageTitle,
+		description: "This is the Game page",
+		theme: "/css/style.css",
+		script: "game.js",
+	},
+};
+
+function callRoute(route) {
+	window.history.pushState({}, "", route);
+	urlLocationHandler();
+}
+
+// create a function that watches the url and calls the urlLocationHandler
+const route = (event) => {
+	event = event || window.event; // get window.event if event argument not provided
+	event.preventDefault();
+	// window.history.pushState(state, unused, target link);
+	window.history.pushState({}, "", event.target.href);
+	urlLocationHandler();
+};
+
+// create a function that handles the url location
+const urlLocationHandler = async () => {
+	const location = window.location.pathname; // get the url path
+	// if the path length is 0, set it to primary page route
+	if ((location.length = 0)) {
+		location = "/";
+	}
+	console.log("location: ", location);
+	// get the route object from the urlRoutes object
+	const route = urlRoutes[location] || urlRoutes["404"];
+	// get the html from the template
+	const html = await fetch(route.template).then((response) =>
+		response.text()
+	);
+	// set the content of the content div to the html
+	document.getElementById("content").innerHTML = html;
+	// set the title of the document to the title of the route
+	document.title = route.title;
+	// set the description of the document to the description of the route
+	document
+		.querySelector('meta[name="description"]')
+		.setAttribute("content", route.description);
+	theme.setAttribute("href", route.theme);
+	if (route.script) {
+		const script = document.createElement("script");
+		script.src = route.script;
+		document.body.appendChild(script);
+	}
+};
+
+// add an event listener to the window that watches for url changes
+window.onpopstate = urlLocationHandler;
+// call the urlLocationHandler function to handle the initial url
+window.route = route;
+// call the urlLocationHandler function to handle the initial url
+urlLocationHandler();
