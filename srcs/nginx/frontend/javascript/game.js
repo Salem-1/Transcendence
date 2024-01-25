9df1c76dd763a31f0fec150f98adc80f748ec6a6
basheer421 <basheer753@gmail.com>
2024-01-25 22:27:56 +0400

    The game is a function

--- srcs/nginx/frontend/javascript/game.js
+++ srcs/nginx/frontend/javascript/game.js
@@ -133,68 +133,85 @@ class Ball {
     }
 }
 
-const p1 = new Paddle(document.getElementById('p1'));
-const p2 = new Paddle(document.getElementById('p2'));
-const ball = new Ball(document.getElementById('ball'));
-let pause = false;
+function handleKeyDown(game) {
+    const {p1, p2} = game;
+    document.addEventListener('keydown', (event) => {
+        const key = event.key.length == 1 ?
+            event.key.toLowerCase() : event.key
+        switch (key) {
+            case 'p':
+            case ' ':
+                game.pause = !game.pause;
+                const pauseElement = document.getElementById('pause');
+                if (game.pause)
+                {
+                    pauseElement.style.setProperty('visibility', 'visible');
+                } else {
+                    pauseElement.style.setProperty('visibility', 'hidden');
+                }
+                break;
+            case 'w':
+                p1.moveUp();
+                break;
+            case 's':
+                p1.moveDown();
+                break;
+            case 'ArrowUp':
+                p2.moveUp();
+                break;
+            case 'ArrowDown':
+                p2.moveDown();
+                break;
+            default:
+                break ;
+        }
+    });
+}
 
-document.addEventListener('keydown', (event) => {
-	const key = event.key.length == 1 ?
-		event.key.toLowerCase() : event.key
-    switch (key) {
-		case 'p':
-		case ' ':
-			pause = !pause;
-			const pauseElement = document.getElementById('pause');
-			if (pause)
-			{
-				pauseElement.style.setProperty('visibility', 'visible');
-			} else {
-				pauseElement.style.setProperty('visibility', 'hidden');
-			}
-			break;
-        case 'w':
-            p1.moveUp();
-            break;
-        case 's':
-            p1.moveDown();
-            break;
-        case 'ArrowUp':
-            p2.moveUp();
-            break;
-        case 'ArrowDown':
-            p2.moveDown();
-            break;
-        default:
-            break ;
+function handleKeyUp(game) {
+    const {p1, p2} = game;
+    document.addEventListener('keyup', (event) => {
+        const key = event.key.length == 1 ?
+            event.key.toLowerCase() : event.key
+        switch (key) {
+            case 'w':
+            case 's':
+                p1.stop();
+                break;
+            case 'ArrowUp':
+            case 'ArrowDown':
+                p2.stop();
+                break;
+        }
+    });
+}
+
+function handleKeyPress(game) {
+    handleKeyDown(game);
+    handleKeyUp(game);
+}
+
+function playGame() {
+    const p1 = new Paddle(document.getElementById('p1'));
+    const p2 = new Paddle(document.getElementById('p2'));
+    const ball = new Ball(document.getElementById('ball'));
+    game = {p1, p2, ball, pause: false};
+
+    handleKeyPress(game);
+
+    let lastTimestamp = 0;
+    function gameLoop(timestamp) {
+        const deltaTime = (timestamp - lastTimestamp) / 1000;
+        if (!game.pause)
+        {
+            p1.update(deltaTime);
+            p2.update(deltaTime);
+            ball.update(deltaTime, p1, p2);
+        }
+        lastTimestamp = timestamp;
+        window.requestAnimationFrame(gameLoop);
     }
-});
-
-document.addEventListener('keyup', (event) => {
-	const key = event.key.length == 1 ?
-		event.key.toLowerCase() : event.key
-    switch (key) {
-        case 'w':
-        case 's':
-            p1.stop();
-            break;
-        case 'ArrowUp':
-        case 'ArrowDown':
-            p2.stop();
-            break;
-    }
-});
-
-let lastTimestamp = 0;
-function gameLoop(timestamp) {
-    const deltaTime = (timestamp - lastTimestamp) / 1000;
-	if (!pause)
-	{
-		p1.update(deltaTime);
-		p2.update(deltaTime);
-		ball.update(deltaTime, p1, p2);
-	}
-    lastTimestamp = timestamp;
     window.requestAnimationFrame(gameLoop);
 }
-window.requestAnimationFrame(gameLoop);
+
+playGame();
