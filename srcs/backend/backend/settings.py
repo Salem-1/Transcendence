7c5dca6a863921755adc935b87966907ea264abe
Salem-1 <ph.ahmedsalem@gmail.com>
2024-02-06 15:48:14 +0400

    fixed the trailing / in our url issue

--- srcs/backend/backend/settings.py
+++ srcs/backend/backend/settings.py
@@ -29,7 +29,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 DEBUG = bool(os.environ.get("DEBUG", default=0))
 
 ALLOWED_HOSTS = ['127.0.0.1', 'localhost']
-
+APPEND_SLASH = False
 
 # Application definition
 
