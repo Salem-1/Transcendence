dc6d7bc1853c9c0ac03080aad806cd525b90cef1
Ahmed Salem <61345528+Salem-1@users.noreply.github.com>
2024-02-09 16:56:42 +0400

    Update add_secrets_theen_delete.py

--- srcs/backend/add_secrets_theen_delete.py
+++ srcs/backend/add_secrets_theen_delete.py
@@ -12,13 +12,7 @@ client = hvac.Client(
 
 path = '/secret/'
 
-secrets_str = """WEB3=fHpwkycBArhYu1ZDkBJMbDDpYpoIbkzCaRZbQo5+Z5B1nbSW7gFVog
-PROJECT_ENDPOINT=https://sepolia.infura.io/v3/456a485f15b54c47bd82fc0e03c395f5
-New_account_created_ADDRESS=0xEcaEB1ed7154fDCA1949b65b78457c87bBA1B3f4
-New_account_Private_key=b'\xa6\xaa^]a\xcb\x95\\\x02\xb7f\xeav\x185\xfa\x8d(uCN\xc7\x8c\xff\xab\x96:\xb4^\xa0v?'
-WALLET_ADDRESS=0x5DeB09f25733e3Bc2052a3d43842Fe0D49f8D57E
-WALLET_PVT_KEY=c4f0c04fd8a8fe29a176596927416fb362f5d6565d61f2a6c02c3effa88287a8
-CONTRACT_ADDRESS=0x5De6e5EC8acbA907DA4A2916286054F720286c7C"""
+secrets_str = """TEST=test"""
 
 secrests_lines = secrets_str.split("\n")
 secrets_len = len(secrests_lines)
